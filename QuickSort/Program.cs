b95549607f7ff7ba7d028b096be881c01a665cf4
aliardan <33637478+aliardan@users.noreply.github.com>
2021-03-22 18:50:35 +0300

    QuickSort implementation

---
+++ QuickSort/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Microsoft.FSharp.Collections;
+using QuickSortFSharpLib;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите элементы массива через пробел и нажмите Enter");
+            var inString = Console.ReadLine();
+            var splittedInString = inString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var intArray = splittedInString.Select(x => Int32.Parse(x));
+            var fSharpIntArray = ListModule.OfSeq(intArray);
+            var sortedArray = QuickSortFSharpLib.QuickSort.Qsort(fSharpIntArray).ToArray();
+            var joinedString = String.Join(' ', sortedArray);
+            Console.WriteLine(joinedString);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QuickSortFSharpLib/Library.fs
@@ -0,0 +1,7 @@
+﻿namespace QuickSortFSharpLib
+
+module QuickSort =
+    let rec Qsort  = function
+    | [] ->  []
+    | pivot::rest -> let left, right = rest |> List.partition(fun i -> i < pivot)
+                     (Qsort left) @ [pivot] @ Qsort right
